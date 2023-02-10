d7b4eb0950bebae0538e0db55eabb2e15ebed5f0
ModdyDev <romeroeinsten@gmail.com>
2023-02-10 15:41:24 -0500

    options.js created

---
+++ options/options.js
@@ -0,0 +1,19 @@
+//Options variables
+    //Customize colors
+let backgroundcolor;
+let textcolor;
+let dividerscolor;
+let bordercolor;
+let datefonttext;
+    //Default Time Zone
+let timezone;
+    //Customize Columns
+let flags;
+let currency;
+let importance;
+let actual;
+let forecast;
+let previus;
+    //Calendar Type
+let ctTimeZone;
+
