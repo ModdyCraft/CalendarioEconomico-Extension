fe8e53212ab31c62bb332f7b12a8fbd3d3de0071
ModdyDev <romeroeinsten@gmail.com>
2023-02-10 19:32:16 -0500

    Refrescando options.js

--- options/options.js
+++ options/options.js
@@ -1,13 +1,27 @@
 //Options variables
     //Customize colors
 let backgroundcolor;
+const ecoDayBackground = "ecoDayBackground=%23" + backgroundcolor;
+
 let textcolor;
+const defaultFont = "defaultFont=%23" + textcolor;
+
 let dividerscolor;
+const innerBorderColor = "innerBorderColor=%23" + dividerscolor;
+
 let bordercolor;
+const borderColor = "borderColor=%23";
+
 let datefonttext;
+const ecoDayFontColor = "ecoDayFontColor=%23";
+
     //Default Time Zone
 let timezone;
+const timeZone = "timeZone=";
+
     //Customize Columns
+const columns = "columns=";
+
 let flags;
 let currency;
 let importance;
@@ -15,5 +29,11 @@ let actual;
 let forecast;
 let previus;
     //Calendar Type
+const calType = "calType=";
+
 let ctTimeZone;
 
+    //Customize Countries
+const customizeCountries = "countries=";
+
+let countries;
