f1c3de15d38d0c91588269f58f467131ded401ca
ModdyDev <romeroeinsten@gmail.com>
2023-02-11 13:13:45 -0500

    Reforced options.js

--- options/options.js
+++ options/options.js
@@ -1,8 +1,9 @@
 //Options variables
 const apply = document.getElementById("nav-apply");
 const colorPickerBackground = document.getElementById("data-background-color");
-const colorPickerTextColor = document.getElementById("data-text-color");
-const colorPickerDividersColor = document.getElementById("data-dividers-color");
+const colorPickerText = document.getElementById("data-text-color");
+const colorPickerDividers = document.getElementById("data-dividers-color");
+const colorPickerBorder = document.getElementById("data-border-color");
 
 const iframeDefaultURL = "https://sslecal2.investing.com?columns=exc_flags,exc_currency,exc_importance,exc_actual,exc_forecast,exc_previous&features=datepicker,timezone&countries=25,32,6,37,72,22,17,39,14,10,35,43,56,36,110,11,26,12,4,5&calType=week&timeZone=8&lang=1";
 
@@ -63,13 +64,17 @@ colorPickerBackground.onchange = function() {
     backgroundcolor = this.value;
 };
 
-colorPickerTextColor.onchange = function() {
+colorPickerText.onchange = function() {
     textcolor = this.value;
 };
 
-colorPickerDividersColor.onchange = function() {
+colorPickerDividers.onchange = function() {
     dividerscolor = this.value;
-}
+};
+
+colorPickerBorder.onchange = function(){
+    bordercolor = this.value;
+};
 
 //Print colors
 apply.onclick = function() {
