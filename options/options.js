1f46ae996c53e934d730770e48027cdb6356acf7
ModdyDev <romeroeinsten@gmail.com>
2023-02-11 09:49:44 -0500

    Cambios simples, nada importante, per con valor

--- options/options.js
+++ options/options.js
@@ -1,4 +1,9 @@
 //Options variables
+const apply = document.getElementById("nav-apply");
+const colorPickerBackground = document.getElementById("data-background-color");
+
+const iframeDefaultURL = "https://sslecal2.investing.com?columns=exc_flags,exc_currency,exc_importance,exc_actual,exc_forecast,exc_previous&features=datepicker,timezone&countries=25,32,6,37,72,22,17,39,14,10,35,43,56,36,110,11,26,12,4,5&calType=week&timeZone=8&lang=1";
+
     //Customize colors
 let backgroundcolor;
 const ecoDayBackground = "ecoDayBackground=%23" + backgroundcolor;
@@ -22,12 +27,8 @@ const timeZone = "timeZone=";
     //Customize Columns
 const columns = "columns=";
 
-let flags;
-let currency;
-let importance;
-let actual;
-let forecast;
-let previus;
+let colunsData;
+    
     //Calendar Type
 const calType = "calType=";
 
@@ -37,3 +38,24 @@ let ctTimeZone;
 const customizeCountries = "countries=";
 
 let countries;
+
+    //Customize Category
+const category = "category=";
+
+let categorys;
+
+    //Customize Importance
+const importance = "importance=";
+
+let importanceData;
+
+    //Feature Selection
+const features = "features=";
+
+let featuresDate;
+
+//Aqui cominza todo
+
+apply.onclick = function() {
+    console.log(colorPickerBackground.getAttribute("value"));
+};
